3dea2302d0944f0d47fafb17c32a2d4b3081ee5a
Adam Natrop <adamnatrop@gmail.com>
2021-04-06 01:01:58 -0500

    cleaned up code formating

--- src/utils.js
+++ src/utils.js
@@ -1,67 +1,44 @@
 const inquirer = require("inquirer");
 
 function getPrompt(type, message, name){
-    // inquirer
-    //     .prompt(
-    //         {
-    //             type: type,
-    //             message: message,
-    //             name: name,
-    //         }
-    //     ).then((response) => {
-    //         name = response.name
-        
-    //     })   
+
     let prompt =
-            {
-                type: type,
-                message: message,
-                name: name,
-            }
-                return prompt
-         
-       
+        {
+            type: type,
+            message: message,
+            name: name,
+        }
+        return prompt         
 }
 
 
 function getRolePrompt(type, message, name){
-    // inquirer
-    //     .prompt(
-    //         {
-    //             type: type,
-    //             message: message,
-    //             name: name,
-    //             choices: ['Manager','Engineer', 'Intern']
-    //         }
-    //     ).then(response => role = response.role)  
+
     let rolePrompt =
-        
         {
-         type: type,
-         message: message,
-         name: name,
-         choices: ['Engineer', 'Intern']
-         }
+            type: type,
+            message: message,
+            name: name,
+            choices: ['Engineer', 'Intern']
+        }
          return rolePrompt;
 }
 
 function getManagerRolePrompt(type, message, name){
  
     let rolePrompt =
-        
         {
-         type: type,
-         message: message,
-         name: name,
-         choices: ['Manager']
-         }
-         return rolePrompt;
+            type: type,
+            message: message,
+            name: name,
+            choices: ['Manager']
+        }
+        return rolePrompt;
 }
 
 function addTeamMember(){
 
     let employeeOptions = 
-
         {
             type: 'list',
             message: 'Would you like to add another team member?',
